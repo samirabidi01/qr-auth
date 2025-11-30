7aa08b639c8bb99d7198370b6370f9d5785fddae
samir <samir.ad19ti@gmail.com>
2025-11-30 22:09:24 +0100

    qr51

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,27 @@
-
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-// Hash password before save
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
+// Hash password before save (Mongoose 7 syntax)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const userModel = mongoose.models.User || mongoose.model("User", userSchema);
-
 export default userModel;
