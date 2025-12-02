10e122046f916f8a4208d75ff80066a028e4ecbe
samir <samir.ad19ti@gmail.com>
2025-12-02 12:59:13 +0100

    qr69

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
    res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: "none",    // required for cross-site (frontend <> backend)
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
